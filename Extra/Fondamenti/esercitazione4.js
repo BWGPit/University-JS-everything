e1e4dc078bbcb47897e29d94c0a031760da1b512
BWGPit <pitoverkill@gmail.com>
2025-11-15 17:54:08 +0100

    Aggiunto l'esercizio 4 dalla esercitazione 4 di Fondamenti

--- Extra/Fondamenti/esercitazione4.js
+++ Extra/Fondamenti/esercitazione4.js
@@ -20,9 +20,19 @@ function min(bt) {
     else return bt.a
 }
 
+function ins(n, bt) {
+    if (!bt) return {t1: null, a: n, t2: null}
+    if (!bt.t1 && n <= bt.a) return {t1: {t1: null, a: n, t2: null}, a: bt.a, t2: bt.t2}
+    else if (!bt.t2 && n >= bt.a) return {t1: bt.t1, a: bt.a, t2: {t1: null, a: n, t2: null}}
+    else if (n <= bt.a) return {t1: ins(n, bt.t1), a: bt.a, t2: bt.t2}
+    else if (n >= bt.a) return {t1: bt.t1, a: bt.a, t2: ins(n, bt.t2)}
+}
+
 function main() {
-    let testBT = {t1: {t1: null, a: 200, t2: null}, a: 25, t2: {t1: null, a: 4, t2: {t1: null, a: 1, t2: null}}}
-    console.log(min(testBT))
+    let t = {t1: {t1: null, a: 2, t2: null}, a: 5, t2: {t1: null, a: 7, t2: null}}
+    let tree2 = {t1: {t1: {t1: null, a: 1, t2: null}, a: 3, t2: {t1: null, a: 5, t2: null}}, a: 8, t2: {t1: null, a: 10, t2: {t1: {t1: null, a: 11, t2: null}, a: 13, t2: null}}}
+    console.log(ins(4, ins(11, tree2)))
+    console.log(ins(11, ins(4, tree2)))
 }
 
 if (require.main === module) {main()}
