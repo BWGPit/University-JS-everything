8c774aac2f75637ef4c3fe079cf02788a85c787b
BWGPit <pitoverkill@gmail.com>
2025-11-17 12:36:58 +0100

    Un esercizio da Evo Learning sulla ricorsione su alberi binari è stato aggiunto

---
+++ Extra/EserciziDaAltreFonti/es1.js
@@ -0,0 +1,38 @@
+/*
+Si consideri una struttura dati ad albero binario, i cui nodi sono
+oggetti con quattro proprietà: sx e dx, che riferiscono rispettivamente
+al figlio sinistro e al figlio destro del nodo stesso; val e sotto, due
+valori interi. Le proprietà sx e dx sono opzionali.
+Si scriva in TS una funzione contaSotto(T) che, dato un albero binario T
+come argomento, conti per ogni nodo t in T (inclusa la radice) il numero
+di nodi appartenenti al sotto-albero di cui t è radice (t incluso). Questo
+valore deve essere scritto in sotto. La funzione restituisce il valore sotto
+della radice di T.
+Nota: Si assuma che il valore di sotto in ogni nodo in T sia inizialmente 0.
+*/
+
+function equal(t1, t2) {
+    if (!t1 || !t2) return (!t1 && !t2)
+    return (
+        t1.val === t2.val &&
+        t1.sotto === t2.sotto &&
+        equal(t1.sx, t2.sx) &&
+        equal(t1.dx, t2.dx)
+    )
+}
+
+function contaSotto(T) {
+    let sottoSx = 0, sottoDx = 0
+    if (T["sx"] != undefined) sottoSx += contaSotto(T.sx)
+    if (T["dx"] != undefined) sottoDx += contaSotto(T.dx)
+    T.sotto = sottoSx + sottoDx + 1
+    return T.sotto
+}
+
+function main() {
+    let Qa = {val: 2, sotto: 0, sx: {val: 4, sotto: 0, sx: {val: 6, sotto: 0}, dx: {val: 6, sotto: 0, dx:{val: 8, sotto: 0}}}, dx: {val: 7, sotto: 0, sx: {val: 8, sotto: 0}}};
+    console.log(contaSotto(Qa))
+    console.log(equal(Qa, {val: 2, sotto: 7, sx: {val: 4, sotto: 4, sx: {val: 6, sotto: 1}, dx: {val: 6, sotto: 2, dx:{val: 8, sotto: 1}}}, dx: {val: 7, sotto: 2, sx: {val: 8, sotto: 1}}}))
+}
+
+if (require.main === module) {main()}
